f701cf0ab46c15a6b0eb9653cf56522487710e82
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-06-22 15:44:32 +0300

    Solved a few more tasks

--- tasks.js
+++ tasks.js
@@ -1,7 +1,8 @@
 //Задача 1
 const findNextSquare = (sq) =>
   Math.sqrt(sq) % 1 === 0 ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
-//Задача 2
+
+  //Задача 2
 const deleteVowels = (str) => {
   const del = "эоайяуе";
   return str
@@ -10,9 +11,17 @@ const deleteVowels = (str) => {
     .join("");
 };
 // const deleteVowels = (str) => str.replace(/[АОУЭИЙЫЕЁЯЮаоуэийыеёяю]/g, "");
-
 // console.log(deleteVowels("Этот сайт для лузеров ЛОЛ!"));
+
 //Задача 3
+const findNumber = (str) => {
+  const arr = str.split(' ')
+  return [
+      Math.min(...arr),
+      Math.max(...arr)
+  ]    
+};
+// console.log(findNumber("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6"));
 
 //Задача 4
 const accum = (str) =>
@@ -24,16 +33,17 @@ const accum = (str) =>
         item.charAt(0).toUpperCase() + item.slice(1)
     )
     .join("-");
-
 // console.log(accum("abcd"));
+
 //Задача 5
 const correctPin = (str) =>
   str.split("").map((item) => isFinite(item)) &&
   (str.length === 4 || str.length === 6);
-
 // console.log(correctPin("5454"));
 // console.log(correctPin("5454666"));
+
 //Задача 6
+
 
 //Задача 7
 const toJadenCase = (str) =>
@@ -43,11 +53,40 @@ const toJadenCase = (str) =>
     .join(" ");
 
 // console.log(toJadenCase("The world is not what we think it is"));
+
 //Задача 8
+const findNumbers = (arr) => {
+  return [
+    Math.min(...arr),
+    Math.max(...arr)
+  ]
+}
+// console.log(findNumbers([1, 5, 8, 10, 35, 100]));
+// console.log(findNumbers([-5, -7, -2, 5]));
+
 //Задача 9
+const simple = (str) => str.split(' ')
+.sort((a, b)=> a.length - b.length)
+.slice(0, 1)
+.join().length
+// console.log(simple('mazda mitsubishi bmw skoda'))
+
 //Задача 10
+const findNeedle = (arr) => `Нашел иголку на позиции ${arr.indexOf('needle')}`;
+// console.log(findNeedle(["bla","wekmvever","needle","bntyn","qwd","qfverv","rtbrtb"]));
+
 //Задача 11
+const invert = (arr) => arr.map(item=> -item)
+// console.log(invert([-1,-2,-4,-5]));
+// console.log(invert([1,2,4,5]));
+// console.log(invert([1,-2,4,-5]));
+
 //Задача 12
+const arrayDiff = (arr1, arr2) => 
+arr1.filter((item)=> !arr2.includes(item))
+// console.log(arrayDiff([1,2],[1]) );
+// console.log(arrayDiff([1,2,2,2,3],[2]));
+
 //Задача 13
 //Задача 14
 //Задача 15
