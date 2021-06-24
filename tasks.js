6bd21bd32357f977dd2eb7f979ae51d30b682213
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-06-24 22:38:58 +0300

    and a little more tasks

--- tasks.js
+++ tasks.js
@@ -2,7 +2,7 @@
 const findNextSquare = (sq) =>
   Math.sqrt(sq) % 1 === 0 ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
 
-  //Задача 2
+//Задача 2
 const deleteVowels = (str) => {
   const del = "эоайяуе";
   return str
@@ -17,9 +17,9 @@ const deleteVowels = (str) => {
 const findNumber = (str) => {
   const arr = str.split(' ')
   return [
-      Math.min(...arr),
-      Math.max(...arr)
-  ]    
+    Math.max(...arr),
+    Math.min(...arr),
+  ].join(' ')
 };
 // console.log(findNumber("4 5 29 54 4 0 -123 666 -64 1 -3 6 -6"));
 
@@ -66,9 +66,9 @@ const findNumbers = (arr) => {
 
 //Задача 9
 const simple = (str) => str.split(' ')
-.sort((a, b)=> a.length - b.length)
-.slice(0, 1)
-.join().length
+  .sort((a, b) => a.length - b.length)
+  .slice(0, 1)
+  .join().length
 // console.log(simple('mazda mitsubishi bmw skoda'))
 
 //Задача 10
@@ -76,54 +76,84 @@ const findNeedle = (arr) => `Нашел иголку на позиции ${arr.i
 // console.log(findNeedle(["bla","wekmvever","needle","bntyn","qwd","qfverv","rtbrtb"]));
 
 //Задача 11
-const invert = (arr) => arr.map(item=> -item)
+const invert = (arr) => arr.map(item => -item)
 // console.log(invert([-1,-2,-4,-5]));
 // console.log(invert([1,2,4,5]));
 // console.log(invert([1,-2,4,-5]));
 
 //Задача 12
-const arrayDiff = (arr1, arr2) => 
-arr1.filter((item)=> !arr2.includes(item))
+const arrayDiff = (arr1, arr2) =>
+  arr1.filter((item) => !arr2.includes(item))
 // console.log(arrayDiff([1,2],[1]) );
 // console.log(arrayDiff([1,2,2,2,3],[2]));
 
 //Задача 13
 
-let it = (n) => n.split().map((item)=> item - 1, 5)
+let it = (n) => n.split().map((item) => item - 1, 5)
 
 // console.log(it(5))
 
 //Задача 14
 const sumMin = (arr) => arr.sort((a, b) => a - b)
-.filter(item => item > 0)
-.slice(0,2)
-.reduce((a, b) => a + b)
+  .filter(item => item > 0)
+  .slice(0, 2)
+  .reduce((a, b) => a + b)
 // console.log(sumMin([12,423,54,1235,3,15,2,-1,52]))
 
 //Задача 15
 const filterStr = (str) => str
-.split(' ')
-.sort((a,b)=>a.split('').filter(item=>
-  isFinite(item))-b.split('').filter(item=>isFinite(item)))
-.join(' ');
+  .split(' ')
+  .sort((a, b) => a.split('').filter(item =>
+    isFinite(item)) - b.split('').filter(item => isFinite(item)))
+  .join(' ');
 
 // console.log(filterStr('is2 Thi1s T4est 3a'));
 
 //Задача 16
 const reverse = (str) => str.split(' ')
-.map((item)=> item.split('').reverse().join('')).join(' ')
+  .map((item) => item.split('').reverse().join('')).join(' ')
 
 // console.log(reverse('This is an example!'))
+
 //Задача 17
+const solution = (str) => str.split(/(?=[A-Z])/)
+  .join('_')
+  .toLowerCase();
+// console.log(solution("redevCourses"));
+
 //Задача 18
+const palindrom = (str) => (str.split(',').reverse().join('')) === str
+// console.log(palindrom('mom'));
+
 //Задача 19
+const countCats = (...arr) => arr.flat(1)
+  .filter((item) => item === '^^').length;
+
+// console.log(countCats([0,1,"^^"],[9,"^^",2],["^^",8,7]))
+
 //Задача 20
+const factorial = (num) => (num != 1) ?
+  num * factorial(num - 1) : 1;
+// console.log(factorial(5));
 //Задача 21
 //Задача 22
 //Задача 23
 //Задача 24
+const isUpperCase = (str, character) => str.charAt(character) === str.charAt(character).toUpperCase();
+// console.log(isUpperCase('tasks JavaScript', 6));
+
 //Задача 25
+const capitalizeFirstLetter = (str) => str.split()
+.map(item => item.charAt(0).toUpperCase() + item.slice(1))
+.join('')
+
+// console.log(capitalizeFirstLetter("пример строки"))
+
 //Задача 26
+const replaceAll = (find, replace, str) => str.split(' ')
+.map(item => item.replace(find, replace)).join(' ')
+
+// console.log(replaceAll('abc', 'x', 'abc def def lom abc abc def'));
 //Задача 27
 //Задача 28
 //Задача 29
